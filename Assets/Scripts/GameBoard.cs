463094932049dbf57763b6726497c60d7105f785
Planete <ytw0113@gmail.com>
2020-07-25 19:56:42 +0900

    Add main level logic scripts

---
+++ Assets/Scripts/GameBoard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoard : MonoBehaviour
+{
+    [SerializeField]
+    private int width;
+    [SerializeField]
+    private int height;
+
+    public Vector2Int board_size
+    {
+        set
+        {
+            width = value.x;
+            height = value.y;
+        }
+
+        get
+        {
+            return new Vector2Int(width, height);
+        }
+    }
+
+    public List<List<>>
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Marble.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marble : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum MarbleType
+{
+    Silver,
+    Gold,
+    White,
+    Blue,
+    Red
+}
+
+public enum ObstacleType
+{
+    Wall,
+    Portal,
+    Lightning
+}
