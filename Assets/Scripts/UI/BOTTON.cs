30bb9f08d8992057d21101e4d3228d60fa813a6c
onnion <rkdwnd333@naver.com>
2020-07-28 16:08:21 +0900

    Main

---
+++ Assets/Scripts/UI/BOTTON.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BOTTON : MonoBehaviour
+{
+    public GameObject ObjectOn;
+    public GameObject ObjectOff;
+    public short Escape = 0;
+
+    public void objon()
+    {
+        ObjectOff.SetActive(false);
+        ObjectOn.SetActive(true);
+    }
+    public void QUIT()
+    {
+        Application.Quit();
+    }
+
+}
+
+
